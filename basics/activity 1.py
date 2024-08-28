6d7a0d8d5a73cc1cab06749142b33dee5967edcd
francisigoe <frankisigoe@gmail.com>
2024-08-28 12:24:55 +0100

    initial commit
    completed python basics activities

---
+++ basics/activity 1.py
@@ -0,0 +1,7 @@
+# made 28/8/24
+# by francis igoe
+# calculates equations using the add, subtract, multiply, devide, modulus operaitors 
+print(-5 + (8 * 6));
+print((55 + 9) % 9);
+print(20 + (-3 * 5) / 8);
+print(5 + 15 / 3 * 2 - 8 % 3);
---
+++ basics/activity 2.py
@@ -0,0 +1,6 @@
+# made 28/8/24 
+# by francis igoe
+# takes 2 inputs from user and divides them
+x = int(input("enter the numerator."));1
+y = int(input("enter the denominator."));
+print(x / y)
