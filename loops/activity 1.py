09c88cce5bd7595392c60038d573c7254645b76e
francisigoe <frankisigoe@gmail.com>
2024-08-28 19:24:11 +0100

    fixed wrong value

--- loops/activity 1.py
+++ loops/activity 1.py
@@ -3,7 +3,7 @@
 # uses a while loop to calculate davinchi seqence two didgets at a time stoping when over a diget is over 50
 val1 = 0
 val2 = 1
-while val1 <= 1000000000:
+while val1 <= 50:
     print(val1)
     print(val2)
     val1 = (val1 + val2)
